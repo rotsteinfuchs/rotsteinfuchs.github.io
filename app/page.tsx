c0d92c3fabb2f9c2e8ac2c5e493d573eb8b064dc
Rotsteinfuchs <169158948+Rotsteinfuchs@users.noreply.github.com>
2025-02-10 15:45:52 +0100

    Create home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,92 +4,47 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <section>
+        <div>
+          <div className={styles.title}>
+            <h1 className={styles.outline}> Hi, I&apos;m Rotsteinfuchs. </h1>
+            <h1> Hi, I&apos;m <span className={styles.highlight}>Rotsteinfuchs</span>. </h1>
+            <h1 className={styles.outline}> Hi, I&apos;m Rotsteinfuchs. </h1>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </section>
+      <section>
+        <div>
+          <h2>About me</h2>
+          <p>
+            I am 14-year-old developer and designer from Germany.<br />
+            I like creating websites, and I&apos;m currently learning<br />
+            <a href="https://www.nextjs.org/">
+              <Image
+                src="/badge_nextjs.svg"
+                alt="NextJS"
+                width={98.75}
+                height={28} />
+            </a>
+            <a href="https://react.dev/">
+              <Image
+                src="/badge_react.svg"
+                alt="React"
+                width={86.25}
+                height={28} />
+            </a>
+            <a href="https://tailwindcss.com/">
+              <Image
+                src="/badge_tailwind.svg"
+                alt="Tailwind"
+                width={113}
+                height={28} />
+            </a><br />
+            I&apos;m currently creating a Minecraft trailer video for <a href="https://www.mctorn.de/">MCTorn</a>.<br />
+            Sometimes I use Python and Java or C++ for my Arduino.
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
