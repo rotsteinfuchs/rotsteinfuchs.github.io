58db14bda7b4d012d8c1004eb6f6b0e78a83abfa
Rotsteinfuchs <169158948+Rotsteinfuchs@users.noreply.github.com>
2025-03-01 14:42:39 +0100

    Remove unnecessary spaces

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { useHeadingUnderline } from "@/hooks/useHeadingUnderline";
 import { useMouseMove } from "@/hooks/useMouseMove";
 import { SocialLinks } from "@/components/SocialLinks";
 import { AboutMe } from "@/components/AboutMe";
-import styles from "@/app/page.module.css";
+import styles from "./page.module.css";
 
 export default function Home() {
   useDiagonalScroll();
@@ -20,9 +20,8 @@ export default function Home() {
             <div className={styles.title}>
               <h1 className={styles.outline}> Hi, I&apos;m Rotsteinfuchs. </h1>
               <h1>
-                {" "}
                 Hi, I&apos;m{" "}
-                <span className={styles.highlight}>Rotsteinfuchs</span>.{" "}
+                <span className={styles.highlight}>Rotsteinfuchs</span>.
               </h1>
               <h1 className={styles.outline}> Hi, I&apos;m Rotsteinfuchs. </h1>
             </div>
