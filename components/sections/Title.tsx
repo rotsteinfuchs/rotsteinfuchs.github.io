42c8ae19f1e52ec13364e90f7a6edb922f44717f
Rotsteinfuchs <169158948+Rotsteinfuchs@users.noreply.github.com>
2025-03-02 13:08:49 +0100

    Fix formatting in Title component (remove unnecessary spaces)

--- components/sections/Title.tsx
+++ components/sections/Title.tsx
@@ -5,12 +5,12 @@ export function Title() {
     <section>
       <div>
         <div className={styles.title}>
-          <h1 className={styles.outline}> Hi, I&apos;m Rotsteinfuchs. </h1>
+          <h1 className={styles.outline}> Hi, I&apos;m Rotsteinfuchs.</h1>
           <h1>
             Hi, I&apos;m <span className={styles.highlight}>Rotsteinfuchs</span>
             .
           </h1>
-          <h1 className={styles.outline}> Hi, I&apos;m Rotsteinfuchs. </h1>
+          <h1 className={styles.outline}> Hi, I&apos;m Rotsteinfuchs.</h1>
         </div>
       </div>
     </section>
